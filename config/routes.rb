48f734fdfcdec31b86ea5df2116e6401e31424c1
sbaixas <sbaixas@gmail.com>
2018-10-07 03:56:37 -0300

    fixed sanitizer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'users/registrations', sessions:'users/sessions'}
+  devise_for :users, :controllers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
