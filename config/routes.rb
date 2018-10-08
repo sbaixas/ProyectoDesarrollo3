f46baee6417659ac27189c996b07bad37c822abe
e440 <jvejar1@miuandes.cl>
2018-10-08 00:33:21 -0300

    Interest Categories : Mobile user can edit and view his interest categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'interesting_categories/edit'
   devise_for :users, :controllers => { registrations: 'users/registrations', sessions:'users/sessions'}
   devise_scope :user do
     post "/users/confirmations", :to => "users/confirmations#activate",   :as => 'activate_user'
   end
+
+  post "/interest_categories/edit", :to =>"interest_categories#update", as:'edit_interest_categories'
+  get "/interest_categories/show", :to =>"interest_categories#show", as:'show_interest_categories'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
